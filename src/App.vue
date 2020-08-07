e734b88fadf9f06d63052727e759bad4e7a09b99
411493321@qq.com <gkchng999>
2020-08-07 18:01:28 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <main-tab></main-tab>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MainTab from 'components/content/mainTabBar/MainTabBar'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    MainTab
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	@import url("assets/css/base.css");
 </style>
